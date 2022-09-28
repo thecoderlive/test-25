368482e74f9758f714bf3525b1a88a646feddd73
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-28 17:57:32 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "419cbcf1-c9e2-424a-942d-ec67a0b572c8",
+    "home_feed": [
+        {
+            "id": "3e48b06e-cbca-4f98-9278-ad4e5c7459d7",
+            "artist_name": "term",
+            "home_carousel": [
+                {
+                    "id": "f86f4bec-5531-4d4a-888d-ba71e3655ef8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/337.png"
+                },
+                {
+                    "id": "ba288622-05f9-4058-9ad2-5cc888d1beeb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/181.png"
+                },
+                {
+                    "id": "89a0c04d-d821-4f61-9c7a-ac5d7517c034",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/200.png"
+                },
+                {
+                    "id": "09db8205-a44a-4448-9dec-3f81dc1cfef6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/146.png"
+                },
+                {
+                    "id": "8c78a1db-3f86-4223-8f94-89ed96b6879d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/220.png"
+                }
+            ],
+            "art_caption": "position",
+            "art_description": "Democrat high cultural anyone. Return fund yet color you still movie. Must view collection tough."
+        },
+        {
+            "id": "3e96c699-8ed2-40e3-b54c-5f03374a167c",
+            "artist_name": "because",
+            "home_carousel": [
+                {
+                    "id": "065e208f-f1a8-4f49-901b-f8144e620663",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/334.png"
+                },
+                {
+                    "id": "cac2e5b8-a664-45d2-8113-35a0d53302fb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/72.png"
+                },
+                {
+                    "id": "d4e1f7d5-f363-47c1-b003-c09f78fe2522",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/96.png"
+                },
+                {
+                    "id": "1a4450d0-e7e3-40ab-b037-40daaa567dbe",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/343.png"
+                },
+                {
+                    "id": "dee875be-7483-475e-a986-93901ffb3cb8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/233.png"
+                }
+            ],
+            "art_caption": "because",
+            "art_description": "Sister expert example trade well win window relate. Impact build help."
+        },
+        {
+            "id": "d5888467-fa02-4569-b1b7-3bf03e86d9ec",
+            "artist_name": "hair",
+            "home_carousel": [
+                {
+                    "id": "ff09db5c-f881-4be9-bd1b-5afd7fe420a6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/246.png"
+                },
+                {
+                    "id": "2d18ea1d-9ee3-46f8-b7ce-c3f2705596bf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/314.png"
+                },
+                {
+                    "id": "20f551a1-63d1-47d2-9308-21e40b2fe246",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/253.png"
+                },
+                {
+                    "id": "89317f68-d3d4-48cd-bd24-5f87bf7afb07",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/106.png"
+                },
+                {
+                    "id": "d172e25b-3f32-4eb6-b4a0-fd9762aa8c2b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/222.png"
+                }
+            ],
+            "art_caption": "group",
+            "art_description": "Bar now nation much."
+        },
+        {
+            "id": "67adc075-8200-4b58-8808-045086c0a609",
+            "artist_name": "group",
+            "home_carousel": [
+                {
+                    "id": "e5f4d007-62db-43de-843e-d4716f10dc73",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/246.png"
+                },
+                {
+                    "id": "fa25d683-ffcb-483f-913a-51852513e424",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/96.png"
+                },
+                {
+                    "id": "fd0b8739-9773-46e1-9b38-7cc04a9ee46c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/212.png"
+                },
+                {
+                    "id": "9d0fce01-120a-4066-a7f8-4be46e01e1ae",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/26.png"
+                },
+                {
+                    "id": "3012523b-0fb4-4a6b-a790-ee5c7d5ed4d1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/148.png"
+                }
+            ],
+            "art_caption": "nation",
+            "art_description": "Type boy million. Seek house worker class Congress begin."
+        },
+        {
+            "id": "0330e57f-6508-4c23-99de-6064760a83de",
+            "artist_name": "structure",
+            "home_carousel": [
+                {
+                    "id": "9e1e6957-33ac-42d7-84de-2a66a9762160",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/180.png"
+                },
+                {
+                    "id": "9ce24947-744f-421d-89bc-9a59e8d33757",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/318.png"
+                },
+                {
+                    "id": "2bf7579a-6665-422a-a9e2-bd56a860eb03",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/365.png"
+                },
+                {
+                    "id": "5317edbb-60ff-4753-bdff-c9edf6fdb9d3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/311.png"
+                },
+                {
+                    "id": "9e9ad003-686a-4f2f-b573-0835cadcd4bd",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/9.png"
+                }
+            ],
+            "art_caption": "ground",
+            "art_description": "Television since increase crime cause citizen. Early bag newspaper test. Oil person control right step."
+        }
+    ]
+}
